97a5fc88d2d4754e980fbc3dacb2ab0834e52a09
Simon Newman <scfnewman@gmail.com>
2019-11-11 10:39:00 +0000

    Add files via upload

---
+++ src/Builtin/C_Light.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef C_LIGHT_H
+#define C_LIGHT_H
+
+#include QuantumAPI
+#include QuantumArchitecture
+
+namespace Quantum
+{
+	class C_Light : public Component
+	{
+	public:
+		float Intensity;
+	};
+}
+
+#endif //!C_LIGHT_T
